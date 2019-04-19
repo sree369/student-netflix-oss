fbc1f45eec7f43a3b33344448b05afc6fdcee427
Sreedhar Palaparthy <sreedharp@gmail.com>
2019-04-19 17:53:30 +0530

    Rename swagger file for student and subject service

--- student-service/src/main/java/org/sree/studentnetflixoss/student/swagger/StudentSwaggerConfig.java
+++ student-service/src/main/java/org/sree/studentnetflixoss/student/swagger/StudentSwaggerConfig.java
@@ -15,7 +15,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
 @EnableSwagger2
-public class SwaggerConfig {
+public class StudentSwaggerConfig {
 
 	public static final Contact DEFAULT_CONTACT = new Contact(
 			"Sreedhar Palaparthy", "", "sreedharp@gmail.com");
